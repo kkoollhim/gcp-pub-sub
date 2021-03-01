eaa98224d5a7342a583e7cdd7dda737ac2fb51c9
Himanshu Singh <hs3045@gmail.com>
2021-03-01 16:50:50 +0530

    Update app.service.ts
    added the topic name.

--- src/app.service.ts
+++ src/app.service.ts
@@ -15,7 +15,13 @@ export class AppService {
 
   async publishData() {
     const dataBuffer = Buffer.from(JSON.stringify(this.data));
-    const messageId =  await pubSubClient.topic("pubsub-demo").publish(dataBuffer); 
-    console.log(`Message ${messageId} published.`);
+    try{
+      const messageId =  await pubSubClient.topic('pubsub-demo').publish(dataBuffer); 
+      console.log(`Message ${messageId} published.`);
+    }
+    catch(error){
+      console.log(`Received error while publishing: ${error.message}`);
+    }
+    
   }
 }
